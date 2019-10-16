d61eba0aefa45ec0bb789569082b0d3b03514092
Evion Cane <evion.cane@sisal.al>
2019-10-16 17:18:26 +0200

    fixing

--- python/year_2019/qualification/dat_bae/Solution.py
+++ python/year_2019/qualification/dat_bae/Solution.py
@@ -1,11 +1,27 @@
 import re
 import math
 
+
 def switch_bit(bit):
     if (bit == '0'):
         return '1'
     elif (bit == '1'):
         return '0'
+
+
+def get_results(node):
+    global count
+    global results
+    if len(node.children) > 0:
+        for child in node.children:
+            get_results(child)
+    elif node.not_working == 0:
+        count += node.length
+    elif node.length == node.not_working:
+        for i in range(0, node.length):
+            results.append(count)
+            count = count + 1
+
 
 class Node(object):
 
@@ -23,29 +39,32 @@ class Node(object):
         if (self.children):
             for child in self.children:
                 inp += child.generate_node_input()
+        elif self.not_working == 0:
+            for i in range(0, int(self.length)):
+                inp += '0'
         else:
             for i in range(0, int(self.length)):
-                if (i%int(self.not_working)==0):
+                if (i % int(self.not_working) == 0):
                     bit = switch_bit(bit)
                 inp += bit
         return inp
+
 
     def generate_child_nodes(self, output):
         global check
         if self.length == 1 or self.not_working == 0 or self.length == self.not_working:
             return
         elif len(self.children) > 0:
-            check = False
             start = int(0)
             for child in self.children:
-                output_length = int(child.length-child.not_working)
+                output_length = int(child.length - child.not_working)
                 end = int(start + output_length)
                 mini_output = output[start: end]
                 child.generate_child_nodes(mini_output)
                 start = start + output_length
         elif int(self.length) > 1 and int(self.not_working) > 0:
             check = False
-            child_node_number = int(self.length/self.not_working)
+            child_node_number = int(self.length / self.not_working)
             my_regex = r"((0){1," + str(self.not_working) + r"}|(1){1," + str(self.not_working) + "})\1*?"
             list = [m.group() for m in re.finditer(my_regex, output)]
             excpected_character = '0'
@@ -53,9 +72,9 @@ class Node(object):
             for i in range(0, child_node_number):
                 if len(list) > i:
                     chr = list[i]
-                #calculate not_working
+                # calculate not_working
                 if chr and chr == excpected_character:
-                    problematic_bits = self.not_working-len(list)
+                    problematic_bits = self.not_working - len(list)
                     node = Node(self.not_working, problematic_bits)
                     self.add_child(node)
                     excpected_character = switch_bit(excpected_character)
@@ -77,9 +96,6 @@ class Node(object):
                         self.add_child(last_node)
 
 
-    #def check_if_done(self):
-
-
 testCases = int(input())
 
 for testCase in range(1, testCases + 1):
@@ -90,11 +106,18 @@ for testCase in range(1, testCases + 1):
     parentNode = Node(n, b)
     for i in range(0, 5):
         check = True
-        #generate input
+        # generate input
         input_param = parentNode.generate_node_input()
         print(input_param, flush=True)
-        #get output
+        # get output
         output = input()
-        #modify tree
+        # modify tree
         parentNode.generate_child_nodes(output)
-        print('KICO')
+        if check:
+            global results
+            results = []
+            global count
+            count = 0
+            get_results(parentNode)
+            print(' '.join(map(str, results)), flush=True)
+            break
