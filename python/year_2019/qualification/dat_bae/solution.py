d8025961598f301716bfc435c4d1108c307d875a
evioncane <evioncane@gmail.com>
2019-10-22 17:46:04 +0200

    fixing stuff

--- python/year_2019/qualification/dat_bae/solution.py
+++ python/year_2019/qualification/dat_bae/solution.py
@@ -1,11 +1,10 @@
 import re
-import math
 
 
 def switch_bit(bit):
-    if (bit == '0'):
+    if bit == '0':
         return '1'
-    elif (bit == '1'):
+    elif bit == '1':
         return '0'
 
 
@@ -36,7 +35,7 @@ class Node(object):
     def generate_node_input(self):
         bit = '1'
         inp = ""
-        if (self.children):
+        if self.children:
             for child in self.children:
                 inp += child.generate_node_input()
         elif self.not_working == 0:
@@ -44,11 +43,10 @@ class Node(object):
                 inp += '0'
         else:
             for i in range(0, int(self.length)):
-                if (i % int(self.not_working) == 0):
+                if i % int(self.not_working) == 0:
                     bit = switch_bit(bit)
                 inp += bit
         return inp
-
 
     def generate_child_nodes(self, output):
         if self.length == 1 or self.not_working == 0 or self.length == self.not_working:
@@ -64,52 +62,46 @@ class Node(object):
         elif int(self.length) > 1 and int(self.not_working) > 0:
             child_node_number = int(self.length / self.not_working)
             my_regex = r"((0){1," + str(self.not_working) + r"}|(1){1," + str(self.not_working) + "})\1*?"
-            list = [m.group() for m in re.finditer(my_regex, output)]
-            excpected_character = '0'
-            #for i in range(0, child_node_number):
+            grouped_by_nodes = [m.group() for m in re.finditer(my_regex, output)]
+            expected = '0'
             i = 0
             while i < child_node_number:
-                if len(list) > i:
-                    chr = list[i][0]
-                # calculate not_working
-                if chr and chr == excpected_character:
-                    problematic_bits = self.not_working - len(list[i])
+                if len(grouped_by_nodes) > i:
+                    actual_char = grouped_by_nodes[i][0]
+                if actual_char and actual_char == expected:
+                    problematic_bits = self.not_working - len(grouped_by_nodes[i])
                     node = Node(self.not_working, problematic_bits)
                     self.add_child(node)
-                    excpected_character = switch_bit(excpected_character)
+                    expected = switch_bit(expected)
                 else:
                     node = Node(self.not_working, self.not_working)
                     self.add_child(node)
-                    if len(list) > i:
+                    if len(grouped_by_nodes) > i:
                         node2 = Node(self.not_working, int(0))
                         self.add_child(node2)
-                        i += 1
-                    excpected_character = switch_bit(excpected_character)
+                        child_node_number -= 1
                 i += 1
             last_node_length = self.length % self.not_working
-            chr = list[-1][0]
+            actual_char = grouped_by_nodes[-1][0]
             if last_node_length != 0:
-                if chr == excpected_character:
-                    last_node_not_working = last_node_length - len(list[-1])
+                if actual_char == expected:
+                    last_node_not_working = last_node_length - len(grouped_by_nodes[-1])
                     last_node = Node(last_node_length, last_node_not_working)
                     self.add_child(last_node)
                 else:
                     previous_node = Node(last_node_length, last_node_length)
                     self.add_child(previous_node)
 
-
     def check_if_solved(self):
         global results
         global count
-        if self.length == 1:
-            return True
-        elif self.not_working == 0:
+        if self.not_working == 0:
+            count += self.length
             return True
         elif self.length == self.not_working:
             for i in range(0, self.length):
                 results.append(count)
                 count += 1
-
             return True
         elif len(self.children) > 0:
             for child in self.children:
@@ -120,29 +112,36 @@ class Node(object):
         return False
 
 
-global results
-global count
+def main():
+    global results
+    global count
+    test_cases = int(input())
+
+    for testCase in range(1, test_cases + 1):
+        solved = False
+        line = input().split()
+        n = line[0]
+        b = line[1]
+        f = line[2]
+        parent_node = Node(n, b)
+        for i in range(0, int(f)):
+            results = []
+            count = 0
+            input_param = parent_node.generate_node_input()
+            print(input_param, flush=True)
+            output = input()
+            parent_node.generate_child_nodes(output)
+            if parent_node.check_if_solved():
+                print(' '.join(map(str, results)), flush=True)
+                test_result = int(input())
+                if test_result == 1:
+                    solved = True
+                else:
+                    raise Exception
+                break
+        if not solved:
+            raise Exception
 
 
-testCases = int(input())
-
-
-for testCase in range(1, testCases + 1):
-    line = input().split()
-    n = line[0]
-    b = line[1]
-    f = line[2]
-    parentNode = Node(n, b)
-    for i in range(0, 5):
-        results = []
-        count = 0
-        # generate input
-        input_param = parentNode.generate_node_input()
-        print(input_param, flush=True)
-        # get output
-        output = input()
-        # modify tree
-        parentNode.generate_child_nodes(output)
-        if parentNode.check_if_solved():
-            print(' '.join(map(str, results)), flush=True)
-            break
+if __name__ == "__main__":
+    main()
